1fdb1db10caee6a9313bcbdefff05b20b1e7df47
Jared Norman <jared@freerunningtechnologies.com>
2014-12-29 19:26:28 -0800

    Fix formatting in mix.exs

--- mix.exs
+++ mix.exs
@@ -2,24 +2,20 @@ defmodule Bugsnag.Mixfile do
   use Mix.Project
 
   def project do
-    [
-      app: :bugsnag,
-      version: "0.1.0-dev",
-      elixir: ">= 0.14.3",
-      package: package,
-      description: """
-        An Elixir interface to the Bugsnag API
-      """,
-      deps: deps
-    ]
+    [app: :bugsnag,
+     version: "0.1.0-dev",
+     elixir: ">= 0.14.3",
+     package: package,
+     description: """
+       An Elixir interface to the Bugsnag API
+     """,
+     deps: deps]
   end
 
   def package do
-    [
-      contributors: ["Jared Norman"],
-      licenses: ["MIT"],
-      links: [github: "https://github.com/jarednorman/bugsnag-elixir"]
-    ]
+    [contributors: ["Jared Norman"],
+     licenses: ["MIT"],
+     links: [github: "https://github.com/jarednorman/bugsnag-elixir"]]
   end
 
   def application do
@@ -27,10 +23,8 @@ defmodule Bugsnag.Mixfile do
   end
 
   defp deps do
-    [
-      {:httpoison, "~> 0.3.0"},
-      {:hackney, "~> 0.13.0", github: "benoitc/hackney"},
-      {:jsex, "~> 2.0.0"}
-    ]
+    [{:httpoison, "~> 0.3.0"},
+     {:hackney, "~> 0.13.0", github: "benoitc/hackney"},
+     {:jsex, "~> 2.0.0"}]
   end
 end
