1a90fae58af17dec488468639f08446881f1e295
Jared Norman <jared@creepywizard.com>
2016-03-25 17:36:10 -0700

    Bump to v1.2.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Bugsnag.Mixfile do
 
   def project do
     [app: :bugsnag,
-     version: "1.1.1",
+     version: "1.2.0",
      elixir: "~> 1.0",
      package: package,
      description: """
