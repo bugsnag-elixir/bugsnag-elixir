5b04c00658d1a2fb789145f0f8f285989361417c
Leszek Zalewski <leszek.zalewski@zalesz.com>
2016-05-09 17:40:00 +0200

    Enable Bugsnag.Logger through configuration option.

--- lib/bugsnag.ex
+++ lib/bugsnag.ex
@@ -7,6 +7,14 @@ defmodule Bugsnag do
   @request_headers [{"Content-Type", "application/json"}]
 
   def start do
+    config = Keyword.merge default_config, Application.get_all_env(:bugsnag)
+
+    if config[:use_logger] do
+      :error_logger.add_report_handler(Bugsnag.Logger)
+    end
+
+    # put normalized api key to application config
+    Application.put_env(:bugsnag, :api_key, config[:api_key])
   end
 
   def report(exception, options \\ []) do
@@ -25,5 +33,12 @@ defmodule Bugsnag do
 
   defp send_notification(body) do
     HTTPoison.post @notify_url, body, @request_headers
+  end
+
+  defp default_config do
+    [
+      api_key: System.get_env("BUGSNAG_API_KEY") || "FAKEKEY",
+      use_logger: true
+    ]
   end
 end
