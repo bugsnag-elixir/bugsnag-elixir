4a64a49d956488fdea425accc9a25374bd937be6
Jorgen Hahn <jhahn@niveon.com>
2016-06-11 11:43:40 -0700

    Read release stage configuration at runtime (instead of at compile time with a module attribute) Set default release stage to "production" instead of "test" Adjust payload test to new default release stage

--- lib/bugsnag/payload.ex
+++ lib/bugsnag/payload.ex
@@ -4,7 +4,6 @@ defmodule Bugsnag.Payload do
     version: Bugsnag.Mixfile.project[:version],
     url: Bugsnag.Mixfile.project[:package][:links][:github],
   }
-  @release_stage Application.get_env(:bugsnag, :release_stage) || "test"
 
   defstruct api_key: nil, notifier: @notifier_info, events: nil
 
@@ -29,7 +28,7 @@ defmodule Bugsnag.Payload do
       |> add_context(Keyword.get(options, :context))
       |> add_user(Keyword.get(options, :user))
       |> add_metadata(Keyword.get(options, :metadata))
-      |> add_release_stage(Keyword.get(options, :release_stage, @release_stage))
+      |> add_release_stage(Keyword.get(options, :release_stage, Application.get_env(:bugsnag, :release_stage, "production")))
 
     Map.put payload, :events, [event]
   end
