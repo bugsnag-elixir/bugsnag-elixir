1d537d7b5bcbdd8d2ddccc2ec17028492b47ce25
Andrew Harvey <andrew@mootpointer.com>
2016-12-20 11:37:20 +1100

    Version bump 1.4.0-beta3

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Bugsnag.Mixfile do
 
   def project do
     [app: :bugsnag,
-     version: "1.4.0-beta2",
+     version: "1.4.0-beta3",
      elixir: "~> 1.0",
      package: package,
      description: """
