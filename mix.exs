68dcf4c9d31b862b7477f419be8ef9486836916f
Dennis Beatty <dennis@dcbeatty.com>
2017-04-29 18:01:09 -0600

    bump poison version

--- mix.exs
+++ mix.exs
@@ -26,7 +26,7 @@ defmodule Bugsnag.Mixfile do
 
   defp deps do
     [{:httpoison, "~> 0.9"},
-     {:poison, "~> 1.5 or ~> 2.0"},
+     {:poison, "~> 1.5 or ~> 2.0 or ~> 3.0"},
      {:ex_doc, ">= 0.0.0", only: :dev},
      {:meck, "~> 0.8.3", only: :test}]
   end
