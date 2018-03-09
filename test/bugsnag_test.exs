691431d983741153e82de32890bc6d0bd744963a
Alex Grant <grantovich@users.noreply.github.com>
2018-03-09 18:06:27 -0500

    Warn and don't send requests if API key is missing

--- test/bugsnag_test.exs
+++ test/bugsnag_test.exs
@@ -37,12 +37,39 @@ defmodule BugsnagTest do
     assert Application.get_env(:bugsnag, :notify_release_stages) == ["production"]
   end
 
+  test "it warns if no api_key is configured" do
+    {:ok, old_key} = Application.fetch_env(:bugsnag, :api_key)
+    Application.delete_env(:bugsnag, :api_key)
+    on_exit fn -> Application.put_env(:bugsnag, :api_key, old_key) end
+
+    assert capture_log(fn -> Bugsnag.start(:temporary, %{}) end) =~ "api_key is not configured"
+  end
+
+  test "it doesn't warn about api_key if the current release stage is not a notifying one" do
+    {:ok, old_stage} = Application.fetch_env(:bugsnag, :release_stage)
+    Application.put_env(:bugsnag, :release_stage, "development")
+    on_exit fn -> Application.put_env(:bugsnag, :release_stage, old_stage) end
+
+    assert capture_log(fn -> Bugsnag.start(:temporary, %{}) end) == ""
+  end
+
   test "it should not explode with logger unset" do
     Application.put_env(:bugsnag, :use_logger, nil)
     on_exit fn -> Application.put_env(:bugsnag, :use_logger, true) end
 
     Bugsnag.start(:temporary, %{})
     assert Application.get_env(:bugsnag, :use_logger) == nil
+  end
+
+  test "warns and returns an error when sending a report with no API key configured" do
+    {:ok, old_key} = Application.fetch_env(:bugsnag, :api_key)
+    Application.delete_env(:bugsnag, :api_key)
+    on_exit fn -> Application.put_env(:bugsnag, :api_key, old_key) end
+
+    log = capture_log fn ->
+      assert {:error, %{reason: "API key is not configured"}} == Bugsnag.sync_report("error!")
+    end
+    assert log =~ "api_key is not configured"
   end
 
   test "does not notify bugsnag if you use sync_report and release_stage is not included in the notify_release_stages" do
