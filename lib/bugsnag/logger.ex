4578a94933cfc2394f347780d57b8d7e3d2f91e8
BlueHotDog <BlueHotDog@users.noreply.github.com>
2018-03-19 13:59:38 +0200

    Replacing Elixir GenEvent with :gen_event to avoid Elixir complaining

--- lib/bugsnag/logger.ex
+++ lib/bugsnag/logger.ex
@@ -2,39 +2,41 @@ defmodule Bugsnag.Logger do
   require Bugsnag
   require Logger
 
-  use GenEvent
+  @behaviour :gen_event
 
-  def init(_mod, []), do: {:ok, []}
+  def init(_init_args), do: {:ok, []}
 
   def handle_call({:configure, new_keys}, _state) do
     {:ok, :ok, new_keys}
   end
 
   def handle_event({_level, gl, _event}, state)
-  when node(gl) != node() do
+      when node(gl) != node() do
     {:ok, state}
   end
 
   def handle_event({:error_report, _gl, {_pid, _type, [message | _]}}, state)
-  when is_list(message) do
+      when is_list(message) do
     try do
       error_info = message[:error_info]
 
       case error_info do
         {_kind, {exception, stacktrace}, _stack} when is_list(stacktrace) ->
           Bugsnag.report(exception, stacktrace: stacktrace)
+
         {_kind, exception, stacktrace} ->
           Bugsnag.report(exception, stacktrace: stacktrace)
       end
     rescue
       ex ->
-        error_type = Exception.normalize(:error, ex).__struct__
-                      |> Atom.to_string
-                      |> String.replace(~r{\AElixir\.}, "")
+        error_type =
+          Exception.normalize(:error, ex).__struct__
+          |> Atom.to_string()
+          |> String.replace(~r{\AElixir\.}, "")
 
         reason = Exception.message(ex)
 
-        Logger.warn "Unable to notify Bugsnag #{error_type}: #{reason}"
+        Logger.warn("Unable to notify Bugsnag #{error_type}: #{reason}")
     end
 
     {:ok, state}
