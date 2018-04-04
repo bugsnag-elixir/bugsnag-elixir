b8f131171606c8171fac9173f931ebf2353ddc20
Coburn Berry <coburn.berry@therealreal.com>
2018-04-03 17:15:41 -0700

    up httpoison version to ~> 1.0

--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule Bugsnag.Mixfile do
   end
 
   defp deps do
-    [{:httpoison, "~> 0.9"},
+    [{:httpoison, "~> 1.0"},
      {:poison, "~> 1.5 or ~> 2.0 or ~> 3.0"},
      {:ex_doc, ">= 0.0.0", only: :dev},
      {:meck, "~> 0.8.3", only: :test}]
