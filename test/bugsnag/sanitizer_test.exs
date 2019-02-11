f5e044b8afba7a3186a9f38029164d0f3f46202b
Grant <grant.powell@salesloft.com>
2019-02-11 13:45:11 -0500

    minimal sanitize test placeholders

---
+++ test/bugsnag/sanitizer_test.exs
@@ -0,0 +1,11 @@
+defmodule Bugsnag.SanitizerTest do
+  use ExUnit.Case
+  import ExUnit.CaptureLog
+
+  test "the sanatizer defaults to not doing anything"
+
+  test "the sanitizer will run the provided functions"
+
+  test "if the sanitizer throws, then we can continue"
+
+end
