41ce39976bd0263fc8d0cc7b8fadfb66c221be1a
nirev <guilherme@nirev.org>
2020-10-05 10:56:13 -0300

    add ensure_loaded to HTTPoison adapter

--- lib/bugsnag/http_client/adapters/httpoison.ex
+++ lib/bugsnag/http_client/adapters/httpoison.ex
@@ -1,26 +1,28 @@
-defmodule Bugsnag.HTTPClient.Adapters.HTTPoison do
-  @moduledoc """
-  HTTPoison adapter for Bugsnag.HTTPClient
-  """
-  alias Bugsnag.HTTPClient
-  alias Bugsnag.HTTPClient.Request
-  alias Bugsnag.HTTPClient.Response
+if Code.ensure_loaded?(HTTPoison) do
+  defmodule Bugsnag.HTTPClient.Adapters.HTTPoison do
+    @moduledoc """
+    HTTPoison adapter for Bugsnag.HTTPClient
+    """
+    alias Bugsnag.HTTPClient
+    alias Bugsnag.HTTPClient.Request
+    alias Bugsnag.HTTPClient.Response
 
-  @behaviour HTTPClient
+    @behaviour HTTPClient
 
-  @impl true
-  def post(%Request{} = request) do
-    request.url
-    |> HTTPoison.post(request.body, request.headers, request.opts)
-    |> case do
-      {:ok, %{body: body, headers: headers, status_code: status}} ->
-        {:ok, Response.new(status, headers, body)}
+    @impl true
+    def post(%Request{} = request) do
+      request.url
+      |> HTTPoison.post(request.body, request.headers, request.opts)
+      |> case do
+        {:ok, %{body: body, headers: headers, status_code: status}} ->
+          {:ok, Response.new(status, headers, body)}
 
-      {:error, %{reason: reason}} ->
-        {:error, reason}
+        {:error, %{reason: reason}} ->
+          {:error, reason}
 
-      _ ->
-        {:error, :unknown}
+        _ ->
+          {:error, :unknown}
+      end
     end
   end
 end
